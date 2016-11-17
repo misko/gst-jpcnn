115345d7900094d211391db77059c4435035d1fd
misko <misko@petbot.com>
2016-11-17 16:32:52 +0800

    enable cropping of motion regions

--- src/gstjpcnn.c
+++ src/gstjpcnn.c
@@ -372,9 +372,11 @@ gst_jpcnn_chain (GstPad * pad, GstObject * parent, GstBuffer * buf)
 	  } else {
 		  network_handle = filter->networkbHandle;
 	  }
-          int do_crop = 0;
-	  if (do_crop==1) {
+          //int do_crop = 0;
+	  //if (do_crop==1) {
+	  if ((filter->detections++)%3==1) {
 		  //lets crop!
+		  
 		  jpcnn_classify_image_wcrop(filter->networkaHandle, imageHandle, flags, filter->layer, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength,meta->x,meta->y,meta->width,meta->height);
 	  } else {
 		  jpcnn_classify_image(filter->networkaHandle, imageHandle, flags, filter->layer, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength);
